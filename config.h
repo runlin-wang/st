ccdea5ae7bae6332ed2ae9266ca4347051066423
alin <alin.run@foxmail.com>
2021-06-20 20:03:33 +0800

    feat: patch openclipboard

--- config.h
+++ config.h
@@ -208,6 +208,7 @@ static Shortcut shortcuts[] = {
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 
+	{ MODKEY,               XK_o,           opencopied,     {.v = "xdg-open"} },
 	{ MODKEY,               XK_l,           copyurl,        {.i =  0} },
 	{ MODKEY,               XK_e,           kscrollup,      {.i =  1} },
 	{ MODKEY,               XK_d,           kscrolldown,    {.i =  1} },
